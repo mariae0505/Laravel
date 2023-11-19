e3e4f6530be6b889638b8c2c8bb88f6aa7f05616
mariae0505 <mariae0505@hotmail.com>
2023-11-19 14:41:20 -0500

    boton papelerax

--- resources/views/abogado/index.blade.php
+++ resources/views/abogado/index.blade.php
@@ -3,64 +3,87 @@
 @section('title', 'El Title')
 
 @section('content_header')
-    <h1>El ContentHeader</h1>
+<style>
+ .trash-icono svg{
+    color: rgb(248, 14, 14) !important;
+}
+    </style>
+<h1>El ContentHeader</h1>
 @stop
 
 @section('content')
-    <a href="abogados/create" class="btn btn-primary mb-3">CREAR ABOGADO</a>
+<a href="abogados/create" class="btn btn-primary mb-3">CREAR ABOGADO</a>
 
-    <table id="abogados" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
+<table id="abogados" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
 
-        <thead class="bg-prymary text-white" >
-            <tr>
-                <th>ID</th>
-                <th>ID TERCERO</th>
-                <th>MAXIMOPROCEOS</th>
-                <th>tarjeta</th>
-                <th>OBSERVACION</th>
-                <th> Opciones</th>
-                
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($abogados as $abogado)
-            <tr>
-                <td>{{$abogado->id}} </td>
-                <td>{{$abogado->terceros->primernombre}} {{$abogado->terceros->primerapellido}}</td>
-                <td>{{$abogado->maximoprocesos}}</td>
-                <td>{{$abogado->tarjeta}}</td>
-                <td>{{$abogado->observaciones}}</td>
-                <td> 
-                    <form action="{{ route ('abogados.destroy',$abogado->id)}}" method="POST">
-                        
-                        <a href="/abogados/{{$abogado->id}}/edit"  class="btn btn-info btn-small" >Editar</a>
+    <thead class="bg-primary text-white ">
+        <tr>
+            <th>ID</th>
+            <th>ID TERCERO</th>
+            <th>MAXIMOPROCEOS</th>
+            <th>tarjeta</th>
+            <th>OBSERVACION</th>
+            <th> Opciones</th>
 
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Borrar</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-    </table>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($abogados as $abogado)
+        <tr>
+            <td>{{$abogado->id}} </td>
+            <td>{{$abogado->terceros->primernombre}} {{$abogado->terceros->primerapellido}}</td>
+            <td>{{$abogado->maximoprocesos}}</td>
+            <td>{{$abogado->tarjeta}}</td>
+            <td>{{$abogado->observaciones}}</td>
+            <td>
+                <form action="{{ route ('abogados.destroy',$abogado->id)}}" method="POST">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <a href="/abogados/{{$abogado->id}}/edit" class="btn btn-primary btn-small"> Editar</a>
+                        </div>
+                    </div>
+                    @csrf
+                    @method('DELETE')
+                    <div class="row">
+                        <div class="col-md-2">
+                            <button type="submit" class="btn btn-light">
+                                <div class="trash-icono">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path
+                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                        <path
+                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                    </svg>
+
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+</table>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="/css/admin_custom.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="public/css/estilos.css">
 @stop
 
 @section('js')
-        <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
 
-        <script>
-            $(document).ready( function () {
+<script>
+    $(document).ready( function () {
                 $('#abogados').DataTable({
                     "lengthMenu": [[5,10,50,-1],[5,10,50,"Todos"]]
     
                 });
             } );
-        </script>
+</script>
 @stop
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
+    <title>LegalWeb</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
@@ -12,124 +13,133 @@
     <!-- Main css -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
     @php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
     @php( $register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register') )
-    @php( $password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset') )
-    
+    @php( $password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url',
+    'password/reset') )
+
     @if (config('adminlte.use_route_url', false))
-        @php( $login_url = $login_url ? route($login_url) : '' )
-        @php( $register_url = $register_url ? route($register_url) : '' )
-        @php( $password_reset_url = $password_reset_url ? route($password_reset_url) : '' )
+    @php( $login_url = $login_url ? route($login_url) : '' )
+    @php( $register_url = $register_url ? route($register_url) : '' )
+    @php( $password_reset_url = $password_reset_url ? route($password_reset_url) : '' )
     @else
-        @php( $login_url = $login_url ? url($login_url) : '' )
-        @php( $register_url = $register_url ? url($register_url) : '' )
-        @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
+    @php( $login_url = $login_url ? url($login_url) : '' )
+    @php( $register_url = $register_url ? url($register_url) : '' )
+    @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
     @endif
 
-    <div class="main">
+    
+        <div class="main">
 
-        
-        <!-- Sing in  Form -->
-        <section class="sign-in" >
-            <div class="container">
-                <div class="signin-content"   >
-                    <div class="signin-image">
-                        <figure><img src="/img/portada2.jpg" alt="sing up image"></figure>
-                        <a href="{{ $register_url }}" class="signup-image-link"> {{ __('adminlte::adminlte.register_a_new_membership') }}</a>
-                        
-                    </div>
 
-                    <div class="signin-form">
-                        <h2 class="form-title">Ingresar</h2>
-                        <form action="{{ $login_url }}" method="post">
-                             @csrf
-                            <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="email" name="email" id="email" class="@error('email') is-invalid @enderror"
-                                value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus/>
+            <!-- Sing in  Form -->
+            <section class="sign-in  ">
+                <div class="container">
+                    <div class="signin-content">
+                        <div class="signin-image">
+                            <figure><img src="/img/portada2.jpg" alt="sing up image"></figure>
+                            <a href="{{ $register_url }}" class="signup-image-link"> {{
+                                __('adminlte::adminlte.register_a_new_membership') }}</a>
+
+                        </div>
+
+                        <div class="signin-form">
+                            <h2 class="form-title">Ingresar</h2>
+                            <form action="{{ $login_url }}" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                    <input type="email" name="email" id="email"
+                                        class="@error('email') is-invalid @enderror" value="{{ old('email') }}"
+                                        placeholder="{{ __('adminlte::adminlte.email') }}" autofocus />
+                                </div>
+
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                <div class="form-group">
+                                    <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                    <input type="password" name="password" id="password"
+                                        class="@error('password') is-invalid @enderror"
+                                        placeholder="{{ __('adminlte::adminlte.password') }}" />
+                                </div>
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+
+
+                                <div class="form-group">
+                                    <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                                    <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember
+                                        me</label>
+                                </div>
+
+
+                                <div class="form-group form-button">
+
+                                    <button type=submit
+                                        class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                                        {{ __('adminlte::adminlte.sign_in') }}
+                                    </button>
+
+
+                                </div>
+
+
+
+
+
+                            </form>
+                            <div class="social-login">
+                                <span class="social-label">Or login with</span>
+                                <ul class="socials">
+                                    <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                                    <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                                    <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                                </ul>
                             </div>
-
-
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-            
-                            <div class="form-group">
-                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password" id="password" class="@error('password') is-invalid @enderror"
-                                placeholder="{{ __('adminlte::adminlte.password') }}"/>
-                            </div>
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-            
-
-
-                            <div class="form-group">
-                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
-                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
-                            </div>
-
-
-                            <div class="form-group form-button">
-
-                                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
-                                    {{ __('adminlte::adminlte.sign_in') }}
-                                </button>
-
-                                
-                            </div>
-
-
- 
-
-
-                        </form>
-                        <div class="social-login">
-                            <span class="social-label">Or login with</span>
-                            <ul class="socials">
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-
-    </div>
+            </section>
+        </div>
+    
 
     @section('auth_footer')
     {{-- Password reset link --}}
     @if($password_reset_url)
-        <p class="my-0">
-            <a href="{{ $password_reset_url }}">
-                {{ __('adminlte::adminlte.i_forgot_my_password') }}
-            </a>
-        </p>
+    <p class="my-0">
+        <a href="{{ $password_reset_url }}">
+            {{ __('adminlte::adminlte.i_forgot_my_password') }}
+        </a>
+    </p>
     @endif
 
     {{-- Register link --}}
     @if($register_url)
-        <p class="my-0">
-            <a href="{{ $register_url }}">
-                {{ __('adminlte::adminlte.register_a_new_membership') }}
-            </a>
-        </p>
+    <p class="my-0">
+        <a href="{{ $register_url }}">
+            {{ __('adminlte::adminlte.register_a_new_membership') }}
+        </a>
+    </p>
     @endif
-@stop
+    @stop
 
 
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
-</body> 
+</body>
+
 </html>
